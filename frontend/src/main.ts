fdddb74eb85de60fd0236a9684bcf84f29aa778f
avinashmsr <avi.synergy.90@gmail.com>
2025-10-07 15:56:22 +0530

    fixing the npm run dev compile time issue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,11 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import Dashboard from './views/Dashboard.vue'
-import Pads from './views/Pads.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Dashboard },
-    { path: '/pads', component: Pads },
+    { path: '/', component: Dashboard }
   ]
 })
 
