c8059fc647c56b8e17ed3f49cf2d9768a5ff2e18
avinashmsr <avi.synergy.90@gmail.com>
2025-10-08 16:43:54 +0530

    adding origins to fix cors issue in browser console

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,10 +9,14 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Rail QMS PoC", version="0.1.0")
 
+origins = [
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+]
 
 app.add_middleware(
 CORSMiddleware,
-allow_origins=["*"],
+allow_origins=origins,
 allow_credentials=True,
 allow_methods=["*"],
 allow_headers=["*"],
