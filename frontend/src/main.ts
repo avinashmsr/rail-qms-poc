97b8d77185f0eaf560057b04c4502318eaf87aab
avinashmsr <avi.synergy.90@gmail.com>
2025-10-08 11:13:23 +0530

    importing and adding Pads component route

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,12 +2,14 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import Dashboard from './views/Dashboard.vue'
+import Pads from './views/Pads.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Dashboard }
-  ]
+    { path: '/', component: Dashboard },
+    { path: '/pads', component: Pads },
+  ],
 })
 
 createApp(App).use(router).mount('#app')
